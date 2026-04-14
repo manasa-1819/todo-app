6b71ef8e81783186375cca3362ebc0cc4871521f
manasa-1819 <manasakonda1914@gmail.com>
2026-04-14 14:10:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>My Todo App</h2>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
